b155357d59c1310601e0b7e995217dfd1ace3d2f
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2024-01-01 23:37:32 +0000

    feat:create RiskTypeController

--- src/main/java/com/edsoncosta/RiskApi/controllers/RiskTypeController.java
+++ src/main/java/com/edsoncosta/RiskApi/controllers/RiskTypeController.java
@@ -1,4 +1,15 @@
 package com.edsoncosta.RiskApi.controllers;
 
+import com.edsoncosta.RiskApi.services.RiskTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/risk")
 public class RiskTypeController {
+
+    @Autowired
+    private RiskTypeService riskTypeService;
+
 }

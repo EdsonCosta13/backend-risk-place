d2df644fb402f5d6b384ea83efdd9a7f52968057
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2024-01-01 23:38:10 +0000

    feat:create RiskTypeService

--- src/main/java/com/edsoncosta/RiskApi/services/RiskTypeService.java
+++ src/main/java/com/edsoncosta/RiskApi/services/RiskTypeService.java
@@ -1,7 +1,29 @@
 package com.edsoncosta.RiskApi.services;
 
+import com.edsoncosta.RiskApi.domain.dtos.RiskTypeDTO;
+import com.edsoncosta.RiskApi.domain.models.RiskType;
+import com.edsoncosta.RiskApi.domain.repositories.RiskTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RiskTypeService {
+
+    @Autowired
+    private RiskTypeRepository riskTypeRepository;
+
+    public RiskType createRiskType(RiskTypeDTO data)
+    {
+        RiskType newRiskType=new RiskType(data);
+        this.saveRiskType(newRiskType);
+
+        return newRiskType;
+    }
+
+    public void saveRiskType(RiskType riskType)
+    {
+        this.riskTypeRepository.save(riskType);
+    }
+
 }

7ffd0532044f980743d1735639830aad2002b084
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2024-01-02 12:38:57 +0000

    feat:create ExceptionHandler

---
+++ src/main/java/com/edsoncosta/RiskApi/infra/RestExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.edsoncosta.RiskApi.infra;
+
+import com.edsoncosta.RiskApi.domain.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    private ResponseEntity threatNotFound (EntityNotFoundException exception)
+    {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    private ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception)
+    {
+        ExceptionDTO exceptionDTO=new ExceptionDTO("Duplication data","400");
+
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(Exception.class)
+    private ResponseEntity threatGenerateException(Exception exception)
+    {
+        ExceptionDTO exceptionDTO=new ExceptionDTO(exception.getMessage(),"500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+}
